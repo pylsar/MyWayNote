8c99997aae4f92a5646262269510fdce81cde7ad
Alex Kusov <kysov2006@bk.ru>
2025-04-23 06:11:45 +0300

    add form styles

--- frontend/src/components/TheAddTaskForm.vue
+++ frontend/src/components/TheAddTaskForm.vue
@@ -1,30 +1,31 @@
 <template>
-    <form @submit.prevent="addTaskData" class="task-form">
-      <input
-        v-model="taskStore.newTask"
-        type="text"
-        placeholder="Введите задачу"
-        class="task-input"
-        :disabled="taskStore.loading"
-      >
-      <textarea
-        v-model="taskStore.newDescription"
-        type="text"
-        placeholder="Введите описание"
-        class="task-input"
-        :disabled="taskStore.loading"
-      ></textarea>
-      <button 
-        type="submit" 
-        class="add-button"
-        :disabled="!taskStore.newTask.trim() || taskStore.loading"
-      >
-        {{ taskStore.loading ? 'Сохранение...' : 'Добавить' }}
-      </button>
-    </form>
-  </template>
+  <form @submit.prevent="addTaskData" class="task-form">
+    <input
+      v-model="taskStore.newTask"
+      type="text"
+      placeholder="Тайтл"
+      class="task-input task-input__title"
+      :disabled="taskStore.loading"
+    >
+    <textarea
+      v-model="taskStore.newDescription"
+      type="text"
+      placeholder="Введите описание"
+      class="task-input task-input__description"
+      :disabled="taskStore.loading"
+    ></textarea>
+    <input type="reset" class="task-input__reset" value="x">
+    <button 
+      type="submit" 
+      class="add-button"
+      :disabled="!taskStore.newTask.trim() || taskStore.loading"
+    >
+      {{ taskStore.loading ? '...' : '+' }}
+    </button>
+  </form>
+</template>
   
-  <script setup>
+<script setup>
   import { useTaskStore } from '@/stores/taskStore'
   
   const taskStore = useTaskStore()
@@ -33,9 +34,73 @@
     taskStore.addTaskData()
   }
 
-  </script>
+</script>
   
-  <style scoped>
- 
+<style lang="scss" scoped>
+ .task-form{
+  display: flex;
+  gap: 8px;
+  & .task-input{
+      color: #7B7B7B;
+      background: #DBE2EF;
+      border: 1px solid #B1B1B1;
+      outline: none;
+      height: 34px;
+      min-height: 34px;
+      border-radius: 6px;
+      resize: none;
+      padding:4px 28px 4px 8px;
+      line-height: 24px;
+      &__title{
+        width: 120px;
+        min-width: 120px;
+      }
+      &__description{
+        flex: 1;
+      }
+      &:focus{
+        outline: none;
+      }
+      &::placeholder{
+        color: #7B7B7B;
+      }
+  }
+  & .add-button{
+    background: #5C9967;
+      border: none;
+    width: 34px;
+    border-radius: 0 6px 6px 0;
+    color: white;
+    cursor: pointer;
+    &:focus{
+      outline: none;
+    
+    }
+  }
+  & .task-input__reset {
+      display: none;
+      position: absolute;
+      right: 56px;
+      top: 8px;
+      width: 24px;
+      height: 34px;
+      outline: none;
+      border: none;
+      color: #FFFFFF;
+      background: transparent;
+      cursor: pointer;
+   }
+  & .task-input__description:not(:placeholder-shown)~.task-input__reset {
+      display: block;
+  }
+
+}
+
+
+
+
+
+
+
   
-  </style>
+</style>
